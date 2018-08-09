90161bfbd1d686b63f3f70aa4bc0ac79c660783c
SatoShun <shun.sato1@gmail.com>
2018-08-09 10:05:17 +0000

    support some types

--- media2/src/main/java/com/github/satoshun/reactivex/media2/RxMediaPlayer2.kt
+++ media2/src/main/java/com/github/satoshun/reactivex/media2/RxMediaPlayer2.kt
@@ -4,6 +4,8 @@ import androidx.annotation.CheckResult
 import androidx.media2.DataSourceDesc2
 import androidx.media2.MediaPlayer2
 import androidx.media2.MediaTimestamp2
+import androidx.media2.SubtitleData2
+import androidx.media2.TimedMetaData2
 import io.reactivex.Observable
 import io.reactivex.Observer
 import java.util.concurrent.Executor
@@ -34,6 +36,37 @@ sealed class MediaPlayer2Event {
     val dsd: DataSourceDesc2,
     val what: Int,
     val extra: Int
+  ) : MediaPlayer2Event()
+
+  data class SubtitleData(
+    val mp: MediaPlayer2,
+    val dsd: DataSourceDesc2,
+    val data: SubtitleData2
+  ) : MediaPlayer2Event()
+
+  data class VideoSizeChanged(
+    val mp: MediaPlayer2,
+    val dsd: DataSourceDesc2,
+    val width: Int,
+    val height: Int
+  ) : MediaPlayer2Event()
+
+  data class TimedMetaDataAvailable(
+    val mp: MediaPlayer2,
+    val dsd: DataSourceDesc2,
+    val data: TimedMetaData2
+  ) : MediaPlayer2Event()
+
+  data class CallCompleted(
+    val mp: MediaPlayer2,
+    val dsd: DataSourceDesc2,
+    val what: Int,
+    val status: Int
+  ) : MediaPlayer2Event()
+
+  data class CommandLabelReached(
+    val mp: MediaPlayer2,
+    val label: Any
   ) : MediaPlayer2Event()
 }
 
@@ -81,5 +114,30 @@ internal class MediaPlayer2Listener(
   override fun onError(mp: MediaPlayer2, dsd: DataSourceDesc2, what: Int, extra: Int) {
     if (isDisposed) return
     observer.onNext(MediaPlayer2Event.Error(mp, dsd, what, extra))
+  }
+
+  override fun onSubtitleData(mp: MediaPlayer2, dsd: DataSourceDesc2, data: SubtitleData2) {
+    if (isDisposed) return
+    observer.onNext(MediaPlayer2Event.SubtitleData(mp, dsd, data))
+  }
+
+  override fun onVideoSizeChanged(mp: MediaPlayer2, dsd: DataSourceDesc2, width: Int, height: Int) {
+    if (isDisposed) return
+    observer.onNext(MediaPlayer2Event.VideoSizeChanged(mp, dsd, width, height))
+  }
+
+  override fun onTimedMetaDataAvailable(mp: MediaPlayer2, dsd: DataSourceDesc2, data: TimedMetaData2) {
+    if (isDisposed) return
+    observer.onNext(MediaPlayer2Event.TimedMetaDataAvailable(mp, dsd, data))
+  }
+
+  override fun onCallCompleted(mp: MediaPlayer2, dsd: DataSourceDesc2, what: Int, status: Int) {
+    if (isDisposed) return
+    observer.onNext(MediaPlayer2Event.CallCompleted(mp, dsd, what, status))
+  }
+
+  override fun onCommandLabelReached(mp: MediaPlayer2, label: Any) {
+    if (isDisposed) return
+    observer.onNext(MediaPlayer2Event.CommandLabelReached(mp, label))
   }
 }
