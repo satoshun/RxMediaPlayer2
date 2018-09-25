91c1937b10d84d50aa0e67ea1d79350be80922b2
SatoShun <shun.sato1@gmail.com>
2018-09-25 12:28:26 +0000

    update libs

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,10 +1,10 @@
 object Vers {
   val compile_sdk = 28
-  val min_sdk = 28 // todo minsdk is Build.VERSION_CODES.KITKAT on new MediaPlayer2
+  val min_sdk = 21
   val target_sdk = 28
-  val agp = "3.1.4"
+  val agp = "3.2.0"
 
-  val kotlin = "1.2.61"
+  val kotlin = "1.2.71"
 }
 
 object Libs {
@@ -15,10 +15,10 @@ object Libs {
 
   val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Vers.kotlin}"
 
-  val appcompat = "androidx.appcompat:appcompat:1.0.0-rc01"
+  val appcompat = "androidx.appcompat:appcompat:1.0.0"
   val constraint_layout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha2"
-  val material = "com.google.android.material:material:1.0.0-rc01"
-  val media2 = "androidx.media2:media2:1.0.0-alpha02"
+  val material = "com.google.android.material:material:1.0.0"
+  val media2 = "androidx.media2:media2:1.0.0-alpha03"
 
   val rxjava = "io.reactivex.rxjava2:rxjava:2.2.0"
   val rxandroid = "io.reactivex.rxjava2:rxandroid:2.0.2"
